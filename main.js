6deeef1c80242729030d043721408d6f46f3a132
SoldatdOR05 <epaleyron@edenschool.fr>
2023-03-31 16:01:42 +0200

    projet finis

---
+++ main.js
@@ -0,0 +1,11 @@
+let modeDark = document.getElementById('modeDark');
+
+modeDark.addEventListener('click', () => {
+    
+})
+
+// modeDark.addEventListener('click', () => {
+//     document.body.innerHTML = "<h2>HA HA HA JE RIGOLE</h2>";
+//     document.body.style.backgroundColor = '#2D3142';
+//     document.body.style.color = '#fff';
+// })
