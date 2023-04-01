26c5338236b63fcb12cf47cf9e30de7b645403dd
SoldatdOR05 <ethan.paleyron@gmail.com>
2023-04-01 19:00:15 +0200

    fin !

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
-let modeDark = document.getElementById('modeDark');
+const modeDark = document.getElementById('modeDark');
+const h2 = document.querySelectorAll("h2");
+const table = document.querySelectorAll("table");
+let bool = true;
 
 modeDark.addEventListener('click', () => {
-    
+    if (bool) {
+        modeDark.innerHTML = "Light";
+        modeDark.style.color = "#000";
+        modeDark.style.backgroundColor = "#EAEBEB";
+        document.body.style.backgroundColor = "#2D3142";
+        bool = false;
+    } else {
+        modeDark.innerHTML = "Dark";
+        modeDark.style.color = "#fff";
+        modeDark.style.backgroundColor = "#2D3142";
+        document.body.style.backgroundColor = "#fff";
+        bool = true;
+    }
+
+    h2.forEach(e => {
+        if (bool) {
+            e.style.color = "#2D3142";
+        } else {
+            e.style.color = "#fff";
+        }
+    })
+
+    table.forEach(e => {
+        if (bool) {
+            e.style.border = "1px solid #2D3142";
+        } else {
+            e.style.border = "1px solid #fff";
+        }
+    })
 })
 
+// C'EST UNE BLAGUE
 // modeDark.addEventListener('click', () => {
-//     document.body.innerHTML = "<h2>HA HA HA JE RIGOLE</h2>";
+//     document.body.innerHTML = "<h2>ha ha c'est une blague </h2>";
 //     document.body.style.backgroundColor = '#2D3142';
 //     document.body.style.color = '#fff';
 // })
